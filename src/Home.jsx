a43fcd76a5b822eb455c7ad4a95f91fe5af37339
Ridham <ridhammakvana99@gmail.com>
2025-04-13 18:58:28 +0530

    component changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -22,7 +22,6 @@ const Home = () => {
                     <div className="all-category-item">
                         <div className="category-item">
                            <Category/>
-                           
                         </div>
                     </div>
                 </div>
