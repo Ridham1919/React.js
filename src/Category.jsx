9d9b023836812e8e93aada6db0e10dbc2ac1f3d9
Ridham <ridhammakvana99@gmail.com>
2025-04-14 11:09:05 +0530

    component changes

--- src/Category.jsx
+++ src/Category.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Category = (props) => {
+const Category = (Props) => {
     return (
         <div className="category-item-inner">
             <div className="img">
