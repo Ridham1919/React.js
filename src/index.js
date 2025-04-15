597fc424a696016eef5d3e9f9b0d7258d96f1260
Ridham <ridhammakvana99@gmail.com>
2025-04-15 17:24:25 +0530

    15-april

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Home';
+import Header from './Header';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
-   <Home/>
+
+
+   <>
+      <BrowserRouter>
+                       <Header />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+         </Routes>
+      </BrowserRouter>
+   </>
+
 );
 
 
