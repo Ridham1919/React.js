5516f476739dee77633d9e2700d761dde8ae7ea6
Ridham <ridhammakvana99@gmail.com>
2025-04-18 16:58:51 +0530

    add component change

--- src/component/Newproduct.jsx
+++ src/component/Newproduct.jsx
@@ -8,10 +8,10 @@ const Newproduct = () => {
             <div className="container">
                 <div className="perfume-main-inner">
                     <div className="perfume-all-item-top">
-                        <Newproductiteam img="asets/new_1.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!"/>
-                        <Newproductiteam img="asets/new_2.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!"/>
-                        <Newproductiteam img="asets/new_1.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!"/>
-                        <Newproductiteam img="asets/new_2.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!"/>
+                        <Newproductiteam img="asets/new_1.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!" price="$232"/>
+                        <Newproductiteam img="asets/new_2.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!" price="$335"/>
+                        <Newproductiteam img="asets/new_1.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!" price="$183"/>
+                        <Newproductiteam img="asets/new_2.jpg" link="perspiciatis unde" new="NEW" sale="ON SALE!!!" price="$241"/>
                     </div>
                 </div>
             </div>
