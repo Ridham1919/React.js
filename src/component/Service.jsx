f21a6f4e134e9c8adab9b5660d343824bbf705c1
Ridham <ridhammakvana99@gmail.com>
2025-04-21 21:27:33 +0530

    add

--- src/component/Service.jsx
+++ src/component/Service.jsx
@@ -9,6 +9,7 @@ const Service = () => {
                 <div className="container">
                     <div className="footer-top-main-inner">
                         <div className="all-content">
+                            
                            <Serviceiteam img="asets/online-support.png" strong="24/7 Online Support" p="Trained staffs are ready to help 24/7"/>
                            <Serviceiteam img="asets/online-support.png" strong="Money Back Guarantee" p="100% money back guarantee"/>
                            <Serviceiteam img="asets/online-support.png" strong="Free Shipping & Return" p="Free shipping on all orders over $100"/>
