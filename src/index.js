87fecb51a05bedebfc5ea1e864bf475a4ec9c5b1
Ridham <ridhammakvana99@gmail.com>
2025-04-28 11:54:41 +0530

    28-april

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ root.render(
            
          </Routes>
                         <Service/>
+                        
                         <Footer/>
                         <Footerend/>
       </BrowserRouter>
