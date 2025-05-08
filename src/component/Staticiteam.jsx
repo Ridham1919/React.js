1e5141c4cb7f9f931d52172222e076feadeb5fde
Ridham <ridhammakvana99@gmail.com>
2025-05-08 10:35:30 +0530

    8-may

--- src/component/Staticiteam.jsx
+++ src/component/Staticiteam.jsx
@@ -3,18 +3,7 @@ import React from "react";
 const Staticiteam = (props) => {
 
     return (
-        <div className="item">
-            <div className="item-inner">
-                <div className="img">
-                    <img src={props.img} alt />
-                </div>
-                <div className="content-img1">
-                    <strong>{props.title}</strong>
-                    <p> {props.line} <br />{props.line2}</p>
-                    <a href="#">shop now</a>
-                </div>
-            </div>
-        </div>
+        
 
         
 
