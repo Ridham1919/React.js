0be0848062fddb6149f6a7d7c569cd94311250c7
Ridham <ridhammakvana99@gmail.com>
2025-05-09 11:09:55 +0530

    9-may

--- src/component/Main_banner.jsx
+++ src/component/Main_banner.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 
 const Main_banner = () => {
-    return (
-        <>
-        <div className="main-banner">
-    <div className="main-banner-container">
-      <div className="main-banner-inner">
-        <div className="all-content">
-          <div className="img">
-            <img src="asets/main.jpg" alt />
-          </div>
-          <div className="img-content">
-            <span>Take a perfume</span>
-            <strong>get <span>25</span>% discount</strong>
-            <p>perfume starting from $9.99</p>
-            <a href="#">shop today</a>
+  return (
+    <>
+      <div className="main-banner">
+        <div className="main-banner-container">
+          <div className="main-banner-inner">
+            <div className="all-content">
+              <div className="img">
+                <img src="asets/main.jpg" alt />
+              </div>
+              <div className="img-content">
+                <span>Take a perfume</span>
+                <strong>get <span>25</span>% discount</strong>
+                <p>perfume starting from $9.99</p>
+                <a href="#">shop today</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-        </>
-    );
+    </>
+  );
 };
 
 export default Main_banner;
