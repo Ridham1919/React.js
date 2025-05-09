42a9a742505e175d902bc6220e3e6d180e77f1b6
Ridham <ridhammakvana99@gmail.com>
2025-05-09 11:31:12 +0530

    9-may

--- src/component/Staticblock.jsx
+++ src/component/Staticblock.jsx
@@ -6,7 +6,6 @@ const Staticblock = () => {
     return (
         <div className="main-static-block">
             <div className="container">
-                
                 <div className="main-static-block-inner">
                     <div className="all-banner-top">
                         <Staticiteam img="asets/1.jpg" title="Popular perfume" line="Use the code" line2="''PERFUME04''"/>
